8aeb86742216f0498e1b10959c7a7168582a6cd9
Terry <123653821@qq.com>
2015-10-16 15:40:48 -0700

    key.css

--- python/server.py
+++ python/server.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -10,12 +9,33 @@ def after(response):
         response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
+@app.route('/')
+def index():
+    return 'Index Page'	
 
-@app.route("/weeby/magic")
+@app.route('/weeby/magic', methods=['GET'])
 def hello_world():
     # Happy hacking :)
-    return "hello, world"
+	if request.method == 'GET':
+		input = request.args.get('spell', '')
+		result = ""
+		dict = {"xyzzy":"fred", "fred":"quux", "quux":"xyzzy"}
+		while(input != ""):
+			if(len(input) >= 4) and (input[0:4] in dict):
+				result += dict.get(input[0:4])
+				input = input[4:]
+			elif(len(input) >= 5) and (input[0:5] in dict):
+				result += dict.get(input[0:5])
+				input = input[5:]
+			else:
+				return "Invalid Input!"
+		return result
+	return "Invalid Input!"
 
+@app.route('/weeby/key.css', methods=['GET'])
+def display_key():
+	if request.method == 'GET':
+		return render_template('key.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=1337)
+    app.run(host='0.0.0.0', port=5000)	
